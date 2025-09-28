2cfe221e21f06417621da28c8fb2328a607030c7
julian-velez <60607869+julian-velez@users.noreply.github.com>
2025-09-28 12:58:10 -0500

    calendar_python
    calendar_python

---
+++ calendar/main.py
@@ -0,0 +1,8 @@
+import calendar   # Importamos el módulo 'calendar' de Python que nos permite trabajar con fechas y calendarios.
+
+año = 2025        # Definimos una variable llamada 'año' y le asignamos el valor 2025.
+mes = 9           # Definimos una variable llamada 'mes' y le asignamos el valor 9 (septiembre).
+
+# Usamos la función 'calendar.month(año, mes)' que devuelve el calendario en texto del mes y año indicados.
+# En este caso, devolverá el calendario de septiembre de 2025 como una cadena formateada.
+print(calendar.month(año, mes))  
